ba87d271ba3aa7c46b49c04cc411d67d205a7193
Bálint Dózsa <balint.do89@gmail.com>
2019-08-12 19:53:25 +0200

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Bálint Dózsa on 2019. 08. 12..
+//  Copyright © 2019. Bálint Dózsa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
